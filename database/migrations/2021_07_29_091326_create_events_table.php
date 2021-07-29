6e213db7abdc75faab8bfa46225b8fd0f41c2e3f
Hayley Simpson <hayleysimpson@Hayleys-MacBook-Air.local>
2021-07-29 10:26:51 +0100

    first commit

---
+++ database/migrations/2021_07_29_091326_create_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->string('description')->nullable(false);
+            $table->dateTime('date')->nullable(false);
+            $table->unsignedBigInteger('organiser_id')->nullable(false);
+            $table->string('location')->nullable(false);
+            $table->unsignedBigInteger('category_id')->nullable(false);
+            $table->string('picture')->nullable(false);
+            $table->integer('interest_ranking')->nullable(false);
+            $table->timestamps();
+
+            $table->foreignId('organiser_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
